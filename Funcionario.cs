7d2d5e31b5226325c9eeaef7bf1cc070c6d5e301
e.serqueira <eduardo.serqueira@outlook.com>
2020-05-25 14:51:16 -0300

    Add project files.

---
+++ Funcionario.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercicio_19
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public void incrementarSalario(double porcentagem)
+        {
+            Salario *= porcentagem;
+        }
+    }
+}
